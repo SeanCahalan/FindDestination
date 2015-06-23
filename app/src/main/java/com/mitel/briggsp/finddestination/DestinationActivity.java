c43e2af83d7ce8725d1b8edf5a1fe1781325ba3d
unknown <briggsp@CA630028.mitel.com>
2015-06-23 11:50:28 -0400

    Initial commit

---
+++ app/src/main/java/com/mitel/briggsp/finddestination/DestinationActivity.java
@@ -0,0 +1,144 @@
+package com.mitel.briggsp.finddestination;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+
+public class DestinationActivity extends Activity implements BeaconConsumer {
+    protected static final String TAG = "Myah";
+    int position = 0;
+    private boolean haveBeenToDestination = false;
+
+    private BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle extras = getIntent().getExtras();
+
+        int placeholderPosition = extras.getInt("position");
+        position = placeholderPosition;
+
+        Log.d(TAG, "trying");
+        setContentView(R.layout.activity_destination);
+        verifyBluetooth();
+
+
+        Region region = new Region ("Destination", null, null, null);
+
+        beaconManager.bind(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(true);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (beaconManager.isBound(this)) beaconManager.setBackgroundMode(false);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                for (Beacon beacon:beacons) {
+                    if((beacon.getId3().toInt() == position + 1) && beacon.getDistance() < 2.0) {
+                        if (!haveBeenToDestination){
+                            logToDisplay("You have reached your destination");
+                            haveBeenToDestination = true;
+                        }
+
+
+                    }
+                }
+
+            }
+
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {
+        }
+    }
+
+    String[] array1;
+
+
+
+    private void verifyBluetooth() {
+
+        try {
+            if (!BeaconManager.getInstanceForApplication(this).checkAvailability()) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Bluetooth not enabled");
+                builder.setMessage("Please enable bluetooth in settings and restart this application.");
+                builder.setPositiveButton(android.R.string.ok, null);
+                builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                        System.exit(0);
+                    }
+                });
+                builder.show();
+            }
+        }
+        catch (RuntimeException e) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Bluetooth LE not available");
+            builder.setMessage("Sorry, this device does not support Bluetooth LE.");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+
+                @Override
+                public void onDismiss(DialogInterface dialog) {
+                    finish();
+                    System.exit(0);
+                }
+
+            });
+            builder.show();
+
+        }
+
+    }
+    public void logToDisplay(final String line) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                EditText editText = (EditText) DestinationActivity.this
+                        .findViewById(R.id.editText);
+                editText.append(line + "\n");
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mitel/briggsp/finddestination/MainActivity.java
@@ -0,0 +1,63 @@
+package com.mitel.briggsp.finddestination;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.Region;
+import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
+import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
+
+
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, BootstrapNotifier {
+    protected static final String TAG = "RogerRoger";
+    private RegionBootstrap regionBootstrap;
+    private BackgroundPowerSaver backgroundPowerSaver;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Region region = new Region("Merp", null, null, null);
+        regionBootstrap = new RegionBootstrap(this, region);
+        backgroundPowerSaver = new BackgroundPowerSaver(this);
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setOnItemClickListener(this);
+    }
+
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.d(TAG, "Here we go");
+        int truePosition = position;
+
+        Intent intent = new Intent(MainActivity.this, DestinationActivity.class);
+        Bundle extras = new Bundle();
+        extras.putInt("position", truePosition);
+        intent.putExtras(extras);
+        this.startActivity(intent);
+
+    }
+
+    @Override
+    public void didEnterRegion(Region ar0) {
+        int p = 2;
+    }
+
+    @Override
+    public void didExitRegion(Region ar0) {
+        int p = 3;
+    }
+
+    @Override
+    public void didDetermineStateForRegion(int state, Region ar0) {
+        int p = 4;
+    }
+
+}
